e3effb45d6df47a19236c2e95521a012bb3376a2
Tamas Mate <tamasmate.dev@gmail.com>
2025-10-21 12:49:08 +0300

    Localization icon fix

--- src/components/ui/SVGComponent.tsx
+++ src/components/ui/SVGComponent.tsx
@@ -3,7 +3,7 @@ import type { SVGComponentProps } from "@/types";
 const SVGComponent = ({ className, viewBox, path, children }: SVGComponentProps) => {
 	return (
 		<svg className={className} viewBox={viewBox} aria-hidden="true" xmlns="http://www.w3.org/2000/svg">
-			{children ?? (path ? <path d={path} /> : null)}
+			{children ?? (path ? <path fillRule="evenodd" clipRule="evenodd" d={path} /> : null)}
 		</svg>
 	);
 };
