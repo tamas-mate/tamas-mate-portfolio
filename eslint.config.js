6aedda2542786cba7a04567666a37096501a5c60
Tamas Mate <tamasmate.dev@gmail.com>
2025-11-26 22:41:08 +0200

    Package and eslint config updates

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,19 @@
 import js from "@eslint/js";
-import pluginESx from "eslint-plugin-es-x";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { globalIgnores } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config([
+export default defineConfig([
 	globalIgnores(["dist"]),
 	{
 		files: ["**/*.{ts,tsx}"],
 		extends: [
 			js.configs.recommended,
 			tseslint.configs.recommended,
-			reactHooks.configs["recommended-latest"],
+			reactHooks.configs.flat.recommended,
 			reactRefresh.configs.vite,
-			pluginESx.configs["flat/restrict-to-es2022"],
 		],
 		languageOptions: {
 			ecmaVersion: 2020,
